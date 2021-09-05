3a5371f8d32c5ffce4023c61d100cbc16dd43241
muhsyafiq <syafiqguitarist@gmail.com>
2021-09-05 18:14:56 +0700

    nul safety
    Null SAFETY

--- lib/bringtoforeground.dart
+++ lib/bringtoforeground.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/services.dart';
 
 class Bringtoforeground {
-  static const MethodChannel _channel =
-      const MethodChannel('bringtoforeground');
+  static const MethodChannel _channel = const MethodChannel('bringtoforeground');
 
   static void bringAppToForeground() {
     _channel.invokeMethod("bringAppToForeground");
